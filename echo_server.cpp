dae488ed5f480426badeea390fcef2ab6a20628d
Ralph Tan <ralphfedericktan@gmail.com>
2025-02-22 01:48:08 -0600

    Added echo_server.cpp

---
+++ echo_server.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+
+//Winsock API for Windows
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#define BUFFER_SIZE 1024
+
+#pragma comment(lib, "Ws2_32.lib") //Add Ws2_#2.lib during the linking process
+
+/*
+The server accepts a port number as a command line argument.
+It creates a TCP socket, binds to the provided port on all network interfaces, and then listens for incoming connections.
+When a client connects, it reads the message and returns the same message back, and closes the connection.
+Then, the server loops back to wait for another client.
+*/
+
+int main (int argc, char* argv[]){
+
+}
