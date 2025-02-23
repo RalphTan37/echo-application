279ac31e451ee59f718b0a61f5b95dbb23e6cc90
Ralph Tan <ralphfedericktan@gmail.com>
2025-02-23 14:17:10 -0600

    Created echo_client.cpp file

---
+++ echo_client.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+
+//Winsock API for Windows
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#define BUFFER_SIZE 1024
+
+#pragma comment(lib, "Ws2_32.lib") //Add Ws2_#2.lib during the linking process
+
+int main (int argc, char* argv[]) {
+    
+}
