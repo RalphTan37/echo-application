b4feaa037d429bf40898e2249a2c2289ef2f81b0
Ralph Tan <ralphfedericktan@gmail.com>
2025-02-23 14:22:45 -0600

    Added Base Case for echo_client.cpp

--- echo_client.cpp
+++ echo_client.cpp
@@ -12,5 +12,12 @@
 #pragma comment(lib, "Ws2_32.lib") //Add Ws2_#2.lib during the linking process
 
 int main (int argc, char* argv[]) {
-    
+    //Ensures the program runs with exactly two arguments, the host name and port number
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <hostname> <port>" << std::endl;
+        return 1;
+    }
+    const char* hostname = argv[1];
+    const char* portStr = argv[2];
+
 }
